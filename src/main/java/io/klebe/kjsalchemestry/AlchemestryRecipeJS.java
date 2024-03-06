3890de4d3b0d94a6ab3391c129f1c3eac997994e
Fabian Rehhauße <f.rehhausse@klebe.io>
2024-03-06 22:53:03 +0100

    Basic KubeJs & Alchemistry Env

---
+++ src/main/java/io/klebe/kjsalchemestry/AlchemestryRecipeJS.java
@@ -0,0 +1,7 @@
+package io.klebe.kjsalchemestry;
+
+import dev.latvian.mods.kubejs.recipe.RecipeJS;
+
+public class AlchemestryRecipeJS extends RecipeJS {
+
+}
---
+++ src/main/java/io/klebe/kjsalchemestry/KubeJSAlchemestry.java
@@ -0,0 +1,10 @@
+package io.klebe.kjsalchemestry;
+
+import net.minecraftforge.fml.common.Mod;
+
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(KubeJSAlchemestry.MODID)
+public class KubeJSAlchemestry {
+    public static final String MODID = "kjsalchemestry";
+}
